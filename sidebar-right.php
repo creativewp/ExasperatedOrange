a6f46f9ee617434c4671b110ca5ae6eb43b9f4c5
Phil <phil.cooling@gmail.com>
2012-08-30 16:38:56 +0100

    Fiz Sibdebar type
    Typo in Role name meant the sidebar wasn't displaying properly - now
    fixed.

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,5 +1,5 @@
 <?php if ( is_sidebar_active('right_sidebar_widget_area') ) : ?>
-        <div id="sidebar" class="widget-area" role="complimentary">
+        <div id="sidebar" class="widget-area" role="complementary">
             <ul class="xoxo">
                 <?php dynamic_sidebar('right_sidebar_widget_area'); ?>
             </ul>
