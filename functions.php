5a1d53b8d5969abd818db2540652eda4b4df4b8f
Phil <phil.cooling@gmail.com>
2012-09-13 16:18:38 +0100

    ENHANCEMENT: Tidy Functions file

--- functions.php
+++ functions.php
@@ -1,16 +1,6 @@
 <?php
 require_once ( get_template_directory() . '/theme-options.php' );
-//add_filter('show_admin_bar', '__return_false');  
 register_nav_menu( 'Left Sidebar', 'Menu for the left sidebar' ); 
-add_theme_support( 'custom-background');
-
-function load_scripts() {
-	//$path = get_bloginfo('template_directory');
-	wp_enqueue_script('jquery');
-	//wp_enqueue_script('theme_javascript',  $path . '/theme.js', array('jquery'));
-}
-add_action('init', 'load_scripts');
-
 
 $header_args = array(
 	'width' => 300,
@@ -18,23 +8,42 @@ $header_args = array(
 	'header-text' => false
 	);
 add_theme_support( 'custom-header', $header_args );
+add_theme_support( 'custom-background');
+add_theme_support( 'menus' );
+
+add_action('init', 'load_scripts');
+add_action('init', 'theme_widgets_init' );
+add_action('admin_menu', 'offcanvas_admin');
+add_action('customize_register', 'offcanvas_customize_register' );
 
 // Make theme available for translation
 // Translations can be filed in the /languages/ directory
 load_theme_textdomain( 'offcanvas-theme', TEMPLATEPATH . '/languages' );
-	
-add_theme_support( 'menus' );
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if ( is_readable($locale_file) )
+  require_once($locale_file);
 
-add_action ('admin_menu', 'themedemo_admin');
 
-function themedemo_admin() {
+
+
+
+
+// Function to load scripts...
+function load_scripts() {
+  //$path = get_bloginfo('template_directory');
+  wp_enqueue_script('jquery');
+  //wp_enqueue_script('theme_javascript',  $path . '/theme.js', array('jquery'));
+}
+
+// Add things to the Admin Menu
+function offcanvas_admin() {
   // add the Customize link to the admin menu
   add_theme_page( 'Customize', 'Customize', 'edit_theme_options', 'customize.php' );
 }
 
-add_action( 'customize_register', 'offcanvas_customize_register' );
+//  Add options to the Customizer screen
 function offcanvas_customize_register($wp_customize) {
-
  // Title Color (added to Color Scheme section in Theme Customizer)
   $wp_customize->add_setting( 'offcanvas_title_color', array(
    // 'default'           => '#000',
@@ -62,17 +71,55 @@ function offcanvas_customize_register($wp_customize) {
   'section'  => 'colors',
   'settings' => 'offcanvas_link_color',
   ) ) );
-
-
-
-
-
 }
 
-$locale = get_locale();
-$locale_file = TEMPLATEPATH . "/languages/$locale.php";
-if ( is_readable($locale_file) )
-  require_once($locale_file);
+
+// Register widgetized areas
+function theme_widgets_init() {
+  // Left Sidebar
+  register_sidebar( array (
+    'name' => 'Left Sidebar Widget Area',
+    'id' => 'left_sidebar_widget_area',
+    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+    'after_widget' => "</li>",
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ) );
+
+  // Right Sidebar
+  register_sidebar( array (
+    'name' => 'Right Sidebar Widget Area',
+    'id' => 'right_sidebar_widget_area',
+    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+    'after_widget' => "</li>",
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ) );
+} // end theme_widgets_init
+
+
+	
+	$preset_widgets = array (
+	    'left_sidebar_widget_area'  => array( 'search', 'meta' ),
+	    'right_sidebar_widget_area'  => array( 'links', 'pages', 'categories', 'archives' )
+	);
+	if ( isset( $_GET['activated'] ) ) {
+	    update_option( 'sidebars_widgets', $preset_widgets );
+	}
+	// update_option( 'sidebars_widgets', NULL );
+	
+	// Check for static widgets in widget-ready areas
+	function is_sidebar_active( $index ){
+	  global $wp_registered_sidebars;
+
+	  $widgetcolums = wp_get_sidebars_widgets();
+
+	  if ($widgetcolums[$index]) return true;
+
+	    return false;
+	} // end is_sidebar_active
+
+
 
 // Get the page number
 function get_page_number() {
@@ -80,6 +127,8 @@ function get_page_number() {
     print ' | ' . __( 'Page ' , 'offcanvas-theme') . get_query_var('paged');
   }
 } // end get_page_number
+
+
 
 	// Custom callback to list comments in the offcanvas-theme style
 	function custom_comments($comment, $args, $depth) {
@@ -173,49 +222,5 @@ function get_page_number() {
 	    return trim(join( $glue, $tags ));
 	} // end tag_ur_it
 	
-	// Register widgetized areas
-	function theme_widgets_init() {
-	    // Area 1
-	    register_sidebar( array (
-	    'name' => 'Left Sidebar Widget Area',
-	    'id' => 'left_sidebar_widget_area',
-	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-	    'after_widget' => "</li>",
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-	  ) );
-
-	    // Area 2
-	    register_sidebar( array (
-	    'name' => 'Right Sidebar Widget Area',
-	    'id' => 'right_sidebar_widget_area',
-	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-	    'after_widget' => "</li>",
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-	  ) );
-	} // end theme_widgets_init
-
-	add_action( 'init', 'theme_widgets_init' );
 	
-	$preset_widgets = array (
-	    'left_sidebar_widget_area'  => array( 'search', 'pages', 'categories', 'archives' ),
-	    'right_sidebar_widget_area'  => array( 'links', 'meta' )
-	);
-	if ( isset( $_GET['activated'] ) ) {
-	    update_option( 'sidebars_widgets', $preset_widgets );
-	}
-	// update_option( 'sidebars_widgets', NULL );
-	
-	// Check for static widgets in widget-ready areas
-	function is_sidebar_active( $index ){
-	  global $wp_registered_sidebars;
-
-	  $widgetcolums = wp_get_sidebars_widgets();
-
-	  if ($widgetcolums[$index]) return true;
-
-	    return false;
-	} // end is_sidebar_active
-
 ?>
